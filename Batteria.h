0ecce412e6b8c1ae8c6f7d30b8405be5d4026642
MATTIA MOLTENI <61653195+mmxeeon@users.noreply.github.com>
2023-02-21 11:25:01 +0100

    Update Batteria.h

--- Batteria.h
+++ Batteria.h
@@ -1,5 +1,27 @@
 #pragma once
-class Batteria
+#include <string>
+using namespace std;
+class batteria
 {
-};
+private:
 
+	int capacita_carica = 0;
+	int livello_carica = 0;
+
+public:
+
+	//costruttore non parametrico
+	batteria() {};
+
+	//costruttore parametrico
+	batteria(int capacita_carica, int livello_carica);
+
+	//metodo per ritornare la capacita_carica
+	int get_capacita_carica();
+
+	//metodo per ritornare il livello_carica
+	int get_livello_carica();
+
+	//metodo stringa per ritornare i valori finali
+	string toString();
+};
